fe9493a49ea7f67326576dcc254b28770a54b3ee
onewby <15062976+onewby@users.noreply.github.com>
2023-01-10 23:46:45 +0000

    Allow building

--- bus-site/src/db.ts
+++ bus-site/src/db.ts
@@ -1,8 +1,4 @@
 import Database from 'better-sqlite3';
-import { fileURLToPath } from 'node:url';
-import {parse as parsePath} from "node:path";
 
-const __file = parsePath(fileURLToPath(import.meta.url))
-
-export const db = new Database(__file.dir + "/../stops.sqlite");
+export const db = new Database("stops.sqlite");
 db.pragma('journal_mode = WAL');
