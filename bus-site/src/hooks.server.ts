d8a15cd675431aa9387cb7edf2c48413b046021e
onewby <15062976+onewby@users.noreply.github.com>
2023-06-23 16:17:32 +0100

    Performance fixes

--- bus-site/src/hooks.server.ts
+++ bus-site/src/hooks.server.ts
@@ -1,3 +1,5 @@
 import {initGTFS} from "./routes/api/service/gtfs-cache";
+import {db} from "./db";
 
+db.open // Doesn't do anything, just ensures the import isn't removed. DB is imported to ensure it is initialised early.
 initGTFS().then(_ => console.log("GTFS downloaded"))
