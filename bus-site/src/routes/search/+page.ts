b900b11b150deca0186e9403aa044327ebbfd58e
onewby <15062976+onewby@users.noreply.github.com>
2024-01-13 16:22:38 +0000

    Search by current location support

--- bus-site/src/routes/search/+page.ts
+++ bus-site/src/routes/search/+page.ts
@@ -2,6 +2,11 @@ import type {PageLoad} from "./$types";
 import {error} from "@sveltejs/kit";
 
 export const load = (({ url }) => {
-    if(!url.searchParams.has("query") || url.searchParams.get("query") === "") error(400, "No query searched for.");
+    if((!url.searchParams.has("query") || url.searchParams.get("query") === "")
+        && (!url.searchParams.has("lat") || url.searchParams.get("lat") === ""
+            || !url.searchParams.has("lon") || url.searchParams.get("lon") === ""))
+        error(400, "No query searched for.");
+    if(isNaN(Number(url.searchParams.get("lat"))) && isNaN(Number(url.searchParams.get("lon"))))
+        error(400, "Invalid lat/lon parameters.")
     return {};
 }) satisfies PageLoad;
